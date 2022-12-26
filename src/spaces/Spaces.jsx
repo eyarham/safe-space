bd5565d8c41b3271a0a2e3c0841ab60d87f40b3b
Eric Yarham <eric.yarham@gmail.com>
2022-12-26 10:46:28 -0500

    removing unused ref

--- src/spaces/Spaces.jsx
+++ src/spaces/Spaces.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SpaceMap from '../map/SpaceMap';
-import { getApprovedDocsSub, getDocsSub } from './api';
+import { getApprovedDocsSub } from './api';
 const Spaces = () => {
   const [markers, setMarkers] = useState([]);
   useEffect(() => {
