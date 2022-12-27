ca99939562fdeae78acf9de391196e8592f87630
Erok21 <eric.yarham@gmail.com>
2022-12-27 09:15:04 -0500

    adding create user, login, logout, account page (#11)

--- src/_common/MenuAppBar.jsx
+++ src/_common/MenuAppBar.jsx
@@ -1,13 +1,15 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UserMenu from '../user/UserMenu';
 
 const MenuAppBar = () => {
 
   return (
-    <div>      
+    <div>
       <Link to={"/"}><h2>Safe Spaces</h2></Link>
       <Link to={"/space"}> <Button variant="outlined">Add New</Button></Link>
+      <UserMenu />
       <hr />
     </div>
   )
