76d812ba880473d5e0ea0483b5b74f7debd2e6ad
Eric Yarham <eric.yarham@gmail.com>
2023-01-01 13:10:36 -0500

    add github api key

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -1,13 +1,14 @@
-
-// const apiKey = (window._env_ && window._env_.FIREBASE_API_KEY) || defaultApiKey;
-// const appId = (window._env_ && window._env_.FIREBASE_APP_ID) || defaultAppId;
+const defaultApiKey = ""//"AIzaSyDl8R22u1-EN2uIqQbgLVg3ylVf1C4v1bE";
+const defaultAppId = ""//"1:191963884767:web:808121b95a4db024a84d6a";
+const apiKey = (window._env_ && window._env_.FIREBASE_API_KEY) || defaultApiKey;
+const appId = (window._env_ && window._env_.FIREBASE_APP_ID) || defaultAppId;
 const firebaseConfig = {
-  apiKey: "AIzaSyDl8R22u1-EN2uIqQbgLVg3ylVf1C4v1bE",
+  apiKey,
   authDomain: "safe-649c4.firebaseapp.com",
   projectId: "safe-649c4",
   storageBucket: "safe-649c4.appspot.com",
   messagingSenderId: "191963884767",
-  appId: "1:191963884767:web:808121b95a4db024a84d6a"
+  appId
 };
 
 export default firebaseConfig;
